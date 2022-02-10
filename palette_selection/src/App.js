a0b17c704981d18f808b62020719d98e71548832
hrishik18 <hrishik.s@somaiya.edu>
2022-02-11 04:42:20 +0530

    clean up

--- palette_selection/src/App.js
+++ palette_selection/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import Colorcard1 from './components/color-card';
+import { ColorPicker1 } from './components/color_picker';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="head">
-         App page 
-      </header>
+    <div >
+         <h1>hello</h1>
+         <ColorPicker1/>
+         <Colorcard1 color="red"/>
     </div>
   );
 }
---
+++ palette_selection/src/components/color-card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Colorcard1(props) {
+  return (
+    <div>
+     {props.color} 
+    </div>
+  )
+}
